e9b4d33986c5727972b474632c6c96ad0e62d271
Pedja <loncarevicpedja2000@gmail.com>
2021-12-28 17:48:45 +0100

    JAVASCRIPT vezba

---
+++ domaciJS1.js
@@ -0,0 +1,5 @@
+a=parseInt(prompt("Unesite prvi broj:"));
+b=parseInt(prompt("Unesite drugi broj:"));
+c=parseInt(prompt("Unesite treci broj:"));
+var max = Math.max(a, b, c);
+alert("Najveci broj je: " +max);
---
+++ domaciJS2.js
@@ -0,0 +1,7 @@
+a=parseInt(prompt("Unesite prvi broj:"));
+b=parseInt(prompt("Unesite drugi broj:"));
+c=parseInt(prompt("Unesite treci broj:"));
+niz=[];
+niz.push(a, b, c);
+niz.sort();
+alert("Uneti brojevi, sortirani neopadajuce su: " +niz);
