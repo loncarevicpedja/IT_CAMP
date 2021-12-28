793b4b795b94a945c5acab055a9d5455842f053f
Pedja <loncarevicpedja2000@gmail.com>
2021-12-28 18:06:34 +0100

    JAVASCRIPT vezba

---
+++ 28.12..js
@@ -0,0 +1,10 @@
+a=parseInt(prompt("Unesite dimenzije prve stranice:"));
+b=parseInt(prompt("Unesite dimenzije druge stranice:"));
+c=parseInt(prompt("Unesite dimenzije trece stranice:"));
+if(a+b>c){
+    alert("Moguce je napreaviti trougao od ovih stranice");
+}
+else{
+    alert("Nije moguce je napreaviti trougao od ovih stranice");
+
+}
