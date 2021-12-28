19652a51cdace412fb324632216660ca614f2db8
Pedja <loncarevicpedja2000@gmail.com>
2021-12-28 18:10:56 +0100

    JAVASCRIPT vezba

--- 28.12..js
+++ 28.12..js
@@ -1,10 +1,10 @@
 a=parseInt(prompt("Unesite dimenzije prve stranice:"));
 b=parseInt(prompt("Unesite dimenzije druge stranice:"));
 c=parseInt(prompt("Unesite dimenzije trece stranice:"));
-if(a+b>c){
-    alert("Moguce je napreaviti trougao od ovih stranice");
+if(a+b<c && (a!==0 || b!==0|| c!==0)){
+    alert("Nije moguce je napreaviti trougao od ovih stranice");
 }
 else{
-    alert("Nije moguce je napreaviti trougao od ovih stranice");
+    alert("Moguce je napreaviti trougao od ovih stranice");
 
 }
