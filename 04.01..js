73c6c9c4c638b413c12049dd72c1313e6d69b1a8
Pedja <loncarevicpedja2000@gmail.com>
2022-01-04 18:11:16 +0100

    JAVASCRIPT vezba

---
+++ 04.01..js
@@ -0,0 +1,22 @@
+var i=1;
+while(i<=50)
+{
+
+    if(i%3===0 && i%5===0)
+    {
+        console.log("Fizz Buzz");
+    }
+    else if(i%3===0)
+    {
+        console.log("Fizz");
+    }
+    else if(i%5===0)
+    {
+        console.log("Buzz");
+    }
+    else
+    {
+        console.log(i)
+    }
+    i++;
+}
