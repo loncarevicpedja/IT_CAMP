7a3fe48ba97b8ca51ac36b88a16844d9436260fd
Pedja <loncarevicpedja2000@gmail.com>
2022-01-04 18:26:16 +0100

    JAVASCRIPT vezba

--- 04.01..js
+++ 04.01..js
@@ -1,22 +1,41 @@
-var i=1;
-while(i<=50)
-{
+// var i=1;
+// while(i<=50)
+// {
 
-    if(i%3===0 && i%5===0)
+//     if(i%3===0 && i%5===0)
+//     {
+//         console.log("Fizz Buzz");
+//     }
+//     else if(i%3===0)
+//     {
+//         console.log("Fizz");
+//     }
+//     else if(i%5===0)
+//     {
+//         console.log("Buzz");
+//     }
+//     else
+//     {
+//         console.log(i)
+//     }
+//     i++;
+// }
+function faktorijel(x)
+{
+    var fact=1;
+    for(let i=1;i<=x;i++)
     {
-        console.log("Fizz Buzz");
+        fact*=i;
     }
-    else if(i%3===0)
-    {
-        console.log("Fizz");
-    }
-    else if(i%5===0)
-    {
-        console.log("Buzz");
-    }
-    else
-    {
-        console.log(i)
-    }
-    i++;
+    return fact;
 }
+var n=parseInt(prompt("Unesite neki broj"));
+var suma=0;
+var c;
+for(let i=1;i<=n;i++)
+{
+    c=faktorijel(i);
+    suma+=c;
+
+}
+console.log(suma);
