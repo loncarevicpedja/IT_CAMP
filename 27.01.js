e06c78c5ca23bd860a43990827c64888e0943e1a
Pedja <loncarevicpedja2000@gmail.com>
2022-01-27 18:41:41 +0100

    javascript vezba

---
+++ 27.01.js
@@ -0,0 +1,31 @@
+// function dvaNajmanja(){
+//     n = [5, 1, 3, 2, 6]
+//     min = n[0];
+//     min1 = n[0];
+//     for(i=0;i<n.length;i++)
+//     {
+//         if(n[i] < min)
+//         {
+//             min = n[i]
+//         }
+//         else if(n[i] < min1 && min1 > min)
+//         {
+//             min1 = n[i]
+//         }
+//     }
+//     console.log(min);
+//     console.log(min1);
+// }
+// dvaNajmanja();
+// function najduziBroj(niz){
+//     max = niz[0];
+//     for(i=0;i<niz.length;i++){
+//         if(max.length > niz[i].length
+//             )
+//         {
+//             max = niz[i];
+//         }
+//     }
+//     console.log(max);
+// }
+// najduziBroj([1,12,123,1234]);
