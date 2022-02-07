488a68e3629628f737142af47234ee005fd7d903
Pedja <loncarevicpedja2000@gmail.com>
2022-02-07 12:11:14 +0100

    javascript vezba

--- hw-site/05.02..js
+++ hw-site/05.02..js
@@ -1,15 +1,35 @@
-niz = [5,3,2,7,1,0]
-var pom
-for(let i=0;i<niz.length;i++)
-{
-    for(let j=i+1;j<niz.length+1;j++)
+// niz = [5,3,2,7,1,0]
+// var pom
+// for(let i=0;i<niz.length;i++)
+// {
+//     for(let j=i+1;j<niz.length+1;j++)
+//     {
+//      if(niz[i] > niz[j])
+//      {
+//          pom = niz[i];
+//          niz[i] = niz[j]
+//          niz[j] = pom
+//      }   
+//     }
+// }
+// console.log(niz);
+
+function toBin(a, b){
+    var c = a + b;
+    var pom = []
+    while(c>0)
     {
-     if(niz[i] > niz[j])
-     {
-         pom = niz[i];
-         niz[i] = niz[j]
-         niz[j] = pom
-     }   
+        if(c % 2 == 0)
+        {
+            
+            pom.push(0)
+        }
+        else{
+            pom.push(1)
+        }
+        c/=2
+        c = Math.ceil(c)
     }
+    console.log(pom);
 }
-console.log(niz);
+toBin(1, 2)
