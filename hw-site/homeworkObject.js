849a67f3cd927694f0c9c5d0b2f71df4b515ea8f
Pedja <loncarevicpedja2000@gmail.com>
2022-02-10 16:16:21 +0100

    javascript vezba object

---
+++ hw-site/homeworkObject.js
@@ -0,0 +1,8 @@
+var Student = {
+    name: "Predrag",
+    surname: "Loncarevic",
+    age: "22"
+}
+console.log("Ime: " + Student.name);
+console.log("Prezime: " + Student.surname);
+console.log("Starost: " + Student.age);
