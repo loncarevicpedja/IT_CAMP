a937cd5b093772699c8fcfb49037c18dbb274bf9
Pedja <loncarevicpedja2000@gmail.com>
2022-02-10 18:18:38 +0100

    javascript vezba object

--- hw-site/homeworkObject.js
+++ hw-site/homeworkObject.js
@@ -1,8 +1,37 @@
-var Student = {
+// var Student = {
+//     name: "Predrag",
+//     surname: "Loncarevic",
+//     age: "22"
+// }
+// console.log("Ime: " + Student.name);
+// console.log("Prezime: " + Student.surname);
+// console.log("Starost: " + Student.age);
+var student = [{
     name: "Predrag",
     surname: "Loncarevic",
-    age: "22"
+    age: "22",
+    status: true,
+},
+{
+    name: "Miljan",
+    surname: "Cosovic",
+    age: "22",
+    status: false,
+},
+{
+    name: "Samil",
+    surname: "Plojovic",
+    age: "22",
+    status: true,
+}]
+
+for(let i = 0; i<student.length;i++)
+{
+    if(student[i].status)
+    {
+        console.log(student[i].name + " " +student[i].surname + ": Procitao")
+    }
+    else{
+        console.log(student[i].name + " " +student[i].surname + ": Nije procitao")
+    }
 }
-console.log("Ime: " + Student.name);
-console.log("Prezime: " + Student.surname);
-console.log("Starost: " + Student.age);
