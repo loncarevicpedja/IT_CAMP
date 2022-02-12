58df894b9b0b4a2b5f28082bab8300623e5129eb
Pedja <loncarevicpedja2000@gmail.com>
2022-02-12 19:11:45 +0100

    javascript vezba object

--- hw-site/10.02..js
+++ hw-site/10.02..js
@@ -1,36 +1,49 @@
-// class Covek{
-//     constructor(ime, prezime, godRodjenja, posao)
-//     {
-//         this.ime = ime;
-//         this.prezime = prezime;
-//         this.godRodjenja = godRodjenja;
-//         this.posao  = posao;
-//     }
-//     starost() {
-//         let datum = new Date();
-//         return datum.getFullYear() - this.godRodjenja;
-//     }
-// } 
-// c1 = new Covek("Miljan", "Cosovic", 2000, "Student")
-// c2 = new Covek("Marko", "Markovic", 2005, "Ucenik")
-// c3 = new Covek("Predrag", "Loncarevic", 2000, "Student")
-// c4 = new Covek("Jovan", "Mrljes", 1996, "Serviser")
-// niz = []
-// niz.push(c1,c2,c3,c4)
-// for(let i =0 ;i<niz.length;i++)
-// {
-//     console.log(niz[i].ime + " " + niz[i].prezime + " ima " + niz[i].starost() + " godina. Zanimanje mu je: " + niz[i].posao + ".");
+// // class Covek{
+// //     constructor(ime, prezime, godRodjenja, posao)
+// //     {
+// //         this.ime = ime;
+// //         this.prezime = prezime;
+// //         this.godRodjenja = godRodjenja;
+// //         this.posao  = posao;
+// //     }
+// //     starost() {
+// //         let datum = new Date();
+// //         return datum.getFullYear() - this.godRodjenja;
+// //     }
+// // } 
+// // c1 = new Covek("Miljan", "Cosovic", 2000, "Student")
+// // c2 = new Covek("Marko", "Markovic", 2005, "Ucenik")
+// // c3 = new Covek("Predrag", "Loncarevic", 2000, "Student")
+// // c4 = new Covek("Jovan", "Mrljes", 1996, "Serviser")
+// // niz = []
+// // niz.push(c1,c2,c3,c4)
+// // for(let i =0 ;i<niz.length;i++)
+// // {
+// //     console.log(niz[i].ime + " " + niz[i].prezime + " ima " + niz[i].starost() + " godina. Zanimanje mu je: " + niz[i].posao + ".");
+// // }
+// o1 = {
+//     name: "Dzemil",
+//     surname: "Dupljak"
 // }
-o1 = {
-    name: "Dzemil",
-    surname: "Dupljak"
-}
-o2 = {}
-let flatObj = Object.entries(o1)
-for (let i = 0; i < flatObj.length; i++) {
+// o2 = {}
+// let flatObj = Object.entries(o1)
+// for (let i = 0; i < flatObj.length; i++) {
         
-    console.log(flatObj[i])
-    o2[flatObj[i][1] = flatObj[i][0]]
+//     console.log(flatObj[i])
+//     o2[flatObj[i][1] = flatObj[i][0]]
+// }
+// console.log(o1)
+// console.log(o2)
+covek = {
+    ime : "Ljanmi",
+    prezime : "Miami",
+    posao : "Student"
 }
-console.log(o1)
-console.log(o2)
+var nizCovek = Object.entries(covek);
+for (let i = 0; i < nizCovek.length; i++) {
+    if(nizCovek[i][0] === "godina")
+    {
+        console.log("Postoji dati prpoerti u klasi")
+    }
+    
+}
---
+++ hw-site/homeworkBookList.js
@@ -0,0 +1,100 @@
+class Book{
+    constructor(title, genre, author){
+        this.title=title;
+        this.genre = genre;
+        this.author = author;
+        this.read = false;
+        this.read_date = null;
+    }
+}
+
+class BookList{
+    constructor(){
+        this.bookShelf = [];
+        this.booksRead = 0;
+        this.booksNotRead = this.bookShelf.length - this.booksRead;
+        this.nextBook = null;
+        this.currBook = null;
+        this.lastBook = null;
+    }
+    
+    add(book){
+        if(book instanceof Book){
+            this.bookShelf.push(book)
+            if(this.currBook)
+            {
+                for (let i = 0; i < this.bookShelf.length; i++) {
+                    if(!this.bookShelf[i].read)
+                    {
+                        this.currBook = this.bookShelf[i]
+                        if(this.bookShelf[i + 1]){
+                            this.nextBook = this.bookShelf[i + 1]
+                        }
+                        else{
+                            this.nextBook = null
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+        else{
+            console.log("invalid argument type")
+        }
+    }
+    finishCurrentBook(){
+        for(let i=0;i<this.bookShelf.length; i++)
+        {
+            const book = this.bookShelf[i]
+            if(book.title = this.bookShelf.title)
+            {
+                this.bookShelf[i].read= true;
+                this.bookShelf[i].read = new Date().now();
+                this.lastBook = this.currBook;
+                this.currBook = this.nextBook;
+                if(this.bookShelf[i + 1]) 
+                {
+                    this.nextBook = this.bookShelf[i + 2];
+                }
+                break;
+            }
+        }
+    }
+}
+
+b1 = new Book("HP1", "magic", "JKR")
+b2 = new Book("HP2", "magic", "JKR")
+b3 = new Book("HP3", "magic", "JKR")
+b4 = new Book("HP4", "magic", "JKR")
+b5 = new Book("HP5", "magic", "JKR")
+
+bookList = new BookList();
+
+bookList.add(b1);
+bookList.add(b2);
+bookList.add(b3);
+bookList.add(b4);
+bookList.add(b5);
+
+console.log(b1.lastBook)
+console.log(b1.currBook)
+console.log(b1.nextBook)
+
+b1.finishCurrentBook();
+
+console.log("------------------------------------------------")
+
+console.log(b1.lastBook)
+console.log(b1.currBook)
+console.log(b1.nextBook)
+
+b1.finishCurrentBook();
+
+console.log("------------------------------------------------")
+
+
+console.log(b1.lastBook)
+console.log(b1.currBook)
+console.log(b1.nextBook)
+
+b1.finishCurrentBook();
