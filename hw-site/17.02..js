c6e8342a2612d45e100cb066a1576e7563cf7ebc
Pedja <loncarevicpedja2000@gmail.com>
2022-02-17 18:21:43 +0100

    javascript vezba object

--- hw-site/17.02..js
+++ hw-site/17.02..js
@@ -1,11 +1,40 @@
-function searchAndDelete(value){
-    objekat = {
-    name : "Miljan",
-    surname : "Cosovic",
-    age : "22",
+function filtriraj(obj, val)
+{
+    pom = []
+    for(let i=0;i<obj.length;i++)
+    {
+        if(obj[i].age < val)
+        {
+            pom.push(obj[i])
+        }
+    }
+    console.log(pom);
 }
-
-delete objekat.value;
-console.log(objekat)
-}
-searchAndDelete("surname");
+biblioteka = [
+    {
+        author : "knjiga1",
+        title : "title1",
+        age : 25,
+    },
+    {
+        author : "knjiga2",
+        title : "title1",
+        age : 23,
+    },
+    {
+        author : "knjiga3",
+        title : "title1",
+        age : 20,
+    },
+    {
+        author : "knjiga4",
+        title : "title1",
+        age : 15,
+    },
+    {
+        author : "knjiga5",
+        title : "title1",
+        age : 27,
+    }
+];
+filtriraj(biblioteka, 23)
