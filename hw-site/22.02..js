7a393a8b347e86e7c1a929c57943b4da1dc7a328
Pedja <loncarevicpedja2000@gmail.com>
2022-02-23 10:02:58 +0100

    javascript vezba foreach

--- hw-site/22.02..js
+++ hw-site/22.02..js
@@ -70,16 +70,9 @@
 
 // console.log(counter)
 
-var myObject = {
-    foo: "bar",
-    func: function() {
-        var self = this;
-        console.log("outer func:  this.foo = " + this.foo);
-        console.log("outer func:  self.foo = " + self.foo);
-        (function() {
-            console.log("inner func:  this.foo = " + this.foo);
-            console.log("inner func:  self.foo = " + self.foo);
-        }());
-    }
-};
-myObject.func();
+(function(){
+  var a = b = 3;
+})();
+
+console.log("a defined? " + (typeof a !== 'undefined'));
+console.log("b defined? " + (typeof b !== 'undefined'));
