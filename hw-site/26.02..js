fabcd057ed877bdf429666f148eeabbe0d75e8e1
Pedja <loncarevicpedja2000@gmail.com>
2022-02-26 18:49:12 +0100

    javascript vezba foreach

---
+++ hw-site/26.02..js
@@ -0,0 +1,11 @@
+niz1 = [1, 3, 2, 1, 2, 2, 3]
+function find(arr1, arr2){
+    arr2.forEach((el) => {
+        if(arr1.includes(el))
+        {
+            arr1.splice(arr1.indexOf(el), 1)
+        }
+    });
+    console.log(arr1)
+}
+find([1, 2, 3, 4], [1, 2, 4])
