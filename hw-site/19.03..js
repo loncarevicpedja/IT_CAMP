9ae26ab78f3620d6500a1b94b7c41e61da7123c6
Pedja <loncarevicpedja2000@gmail.com>
2022-03-22 19:04:23 +0100

    a

---
+++ hw-site/19.03..js
@@ -0,0 +1,27 @@
+window.onload=async function () {
+    pokemonResource=[]
+    pokemonInfo=[]
+    const getPokemonList = async () => {
+        try {
+            const r = await fetch("https://pokeapi.co/api/v2/pokemon/?offset=10&limit=10");
+            const res = await r.json()
+            pokemonResource = res;
+        } catch (error) {
+            console.log(error)
+        }
+    };
+    const getPokemon = async (URL) => {
+        try {
+            const p = await fetch(URL)
+            const pokemon = await p.json()
+            pokemonInfo.push(pokemon)
+        } catch (error) {
+            console.log(error)
+        }
+    };
+    await getPokemonList();
+    pokemonArr = pokemonResource.result.map((p) => {
+        return getPokemon(p.url);
+    });
+    await Promise.all(pokemonArr);
+};
