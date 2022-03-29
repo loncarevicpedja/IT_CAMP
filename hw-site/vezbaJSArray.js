ab98364c9249792c562c3cd0bd19bb84a88abdbb
Pedja <loncarevicpedja2000@gmail.com>
2022-03-29 23:25:47 +0200

    a

--- hw-site/vezbaJSArray.js
+++ hw-site/vezbaJSArray.js
@@ -1,17 +1,32 @@
-class Nesto{
-    constructor(ime, prezime, godine){
-        this.ime=ime;
-        this.prezime=prezime;
-        this.godine=godine;
+// class Nesto{
+//     constructor(ime, prezime, godine){
+//         this.ime=ime;
+//         this.prezime=prezime;
+//         this.godine=godine;
+//     }
+//     punoletean() {
+//         if(this.godine > 18)
+//         return "jeste"
+//         else
+//         return "nije"
+//     }
+// }
+// var covek = new Nesto("Marko", "Markovic", 18)
+// console.log(covek)
+// a = covek.punoletean(); 
+// console.log(a)
+
+let p = new Promise((reslove, reject) => {
+    let a = 3 + 1
+    if(a == 2){
+        reslove("2")
     }
-    punoletean() {
-        if(this.godine > 18)
-        return "jeste"
-        else
-        return "nije"
+    else{
+        reject("nije 2")
     }
-}
-var covek = new Nesto("Marko", "Markovic", 18)
-console.log(covek)
-a = covek.punoletean(); 
-console.log(a)
+})
+p.then((poruka) => {
+    console.log("Rezultat je " + poruka)
+}).catch((poruka) => {
+    console.log("Rezultat " + poruka)
+})
