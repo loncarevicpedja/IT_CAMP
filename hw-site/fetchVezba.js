a87def1fc1bd0cf2c559f6a06add625855086c66
Pedja <loncarevicpedja2000@gmail.com>
2022-04-06 14:06:25 +0200

    fatch

--- hw-site/fetchVezba.js
+++ hw-site/fetchVezba.js
@@ -1,53 +1,61 @@
+var link = "https://catfact.ninja/facts"
 function fetchData(){
-    var fetchLink="https://catfact.ninja/facts"
-    fetch(fetchLink)
-    .then((res) => {
-        return res.json()
+    fetch(link)
+    .then((podatak) => {
+        return podatak.json()
     })
-    .then((res) => {
-        for (let i = 0; i < res.data.length; i++) {
-            glDiv.appendChild(generateCard((res.data[i])))
+    .then((podatak) => {
+        console.log(podatak)
+        gla.innerHTML = ""
+        for (let i = 0; i < podatak.data.length; i++) {
+            generateCard(podatak.data[i])        
         }
-        console.log(res)
-        for (let i = 0; i < res.data.length; i++) {
-            pagination.appendChild(generatePage((res.data[i])))
+        paginacija.innerHTML = ""
+        for (let i = 0; i < podatak.links.length; i++) {
+            generatePage(podatak.links[i])        
         }
-        
     })
     .catch((err) => {
         console.log(err)
     })
 }
-var glDiv = document.getElementById('glavni')
-glDiv.style.display = "flex"
-glDiv.style.flexDirection = "row"
-glDiv.style.flexWrap = "wrap"
-var pagination = document.getElementById('paginacija')
-pagination.style.display = "flex"
-pagination.style.flexDirection = "row"
-pagination.style.backgroundColor = "silver"
-pagination.style.width = "97wh"
-pagination.style.height = "50px"
-pagination.style.alignItems = "center"
-pagination.style.justifyContent = "center"
-
-
-function generateCard(podatak){
-    var kartica = document.createElement('div')
-    kartica.style.background= "skyblue"
-    kartica.style.height = "100px"
-    kartica.style.width = "300px"
-    kartica.style.margin = "20px"
-    kartica.innerHTML = podatak.fact
+function generateCard(vals){
+    kartica = document.createElement('div')
+    kartica.style.backgroundColor = "red"
+    kartica.style.width = "200px"
+    kartica.style.height = "300px"
+    kartica.innerHTML = vals.fact;
+    var glavni = document.getElementById('glavni')
+    glavni.appendChild(kartica)
     return kartica
 }
-function generatePage(podatak){
-    var kartica = document.createElement('div')
-    kartica.style.background= "skyblue"
-    kartica.style.height = "100px"
-    kartica.style.width = "300px"
-    kartica.style.margin = "20px"
-    kartica.innerHTML = podatak.links.label
-    return kartica
+function generatePage(vals){
+    page = document.createElement('button')
+    page.innerHTML = vals.label
+    page.onclick = function (e)
+    {
+        e.preventDefault()
+        link = vals.url
+        fetchData()
+    }
+    page.style.backgroundColor = "red"
+    page.innerHTML = vals.label;
+    var gl = document.getElementById('paginacija')
+    gl.appendChild(page)
+    return page
 }
+gla = document.getElementById('glavni')
+gla.style.display = "flex"
+gla.style.flexDirection = "row"
+gla.style.gap = "20px"
+gla.style.flexWrap = "wrap"
+
+paginacija = document.getElementById('paginacija')
+paginacija.style.width = "97vw"
+paginacija.style.height = "70px"
+paginacija.style.display = "flex"
+paginacija.style.flexDirection = "row"
+paginacija.style.alingItem = "center"
+paginacija.style.justifyContent = "center"
+paginacija.style.backgroundColor = "silver"
 fetchData()
